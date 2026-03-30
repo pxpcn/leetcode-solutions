b2b951edb1d0db8ce96cc5dd46eb313442beaee6
pxpcn <nickita.popcko@yandex.ru>
2026-03-30 17:05:56 +0300

    Create Climbing Stairs.py

---
+++ Climbing Stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return n
+        else:
+            a, b = 1, 2
+            for i in range(3, n + 1):
+                a, b = b, a + b
+            return b
+            
